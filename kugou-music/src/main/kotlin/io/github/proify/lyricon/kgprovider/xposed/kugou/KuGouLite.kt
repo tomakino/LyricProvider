1eee4fb7b07edf0760c7caee6f27c015f2f9e095
dhw12 <97172401+dhw12@users.noreply.github.com>
2026-05-10 00:14:44 +0800

    Delete kugou-music directory

--- kugou-music/src/main/kotlin/io/github/proify/lyricon/kgprovider/xposed/kugou/KuGouLite.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2026 Proify, Tomakino
- * Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0
- */
-
-package io.github.proify.lyricon.kgprovider.xposed.kugou
-
-class KuGouLite : KuGou()
